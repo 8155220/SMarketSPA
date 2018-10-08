90656801ffb9524f85abfb13f1ba8586080e4316
Shep <alfredochsoliz@gmail.com>
2018-10-08 15:19:14 -0400

    funcionando crear

--- src/app/services/smarket.service.ts
+++ src/app/services/smarket.service.ts
@@ -107,7 +107,7 @@ export class SMarketService {
       })
       .subscribe(resp => console.log(resp));
   }*/
-
+/*
   async createProduct(product: any,eventTarget:any){
     let postUrl: string = `${this.url}Products/`;
     let headers = new HttpHeaders().set("Content-Type", "application/json");
@@ -143,10 +143,59 @@ export class SMarketService {
     }); 
       }
       );
-    
-      
-      
   }
+*/
+async createProduct(product: any,eventTarget:any){
+  let postUrl: string = `${this.url}Products/`;
+  let headers = new HttpHeaders().set("Content-Type", "application/json");
+
+  let productCreated:any; 
+ 
+
+     let counter:number = 0;
+    eventTarget.forEach(e => {
+      const filePath = `productImages/+${new Date().getTime()}_${
+        product.name
+      }`;
+      const fileRef = this.storage.ref(filePath);
+      const task = this.storage.upload(filePath, e);
+      this.uploadPercent = task.percentageChanges();
+      task
+        .snapshotChanges()
+        .pipe(
+          finalize(() => {
+            fileRef.getDownloadURL().subscribe(urlfile => {
+              counter++;
+              console.log('Contador :'+counter);
+              var image = {url:urlfile};
+              this.images.push(image);
+              if (counter==4){
+                product.images=this.images;
+                product.image=this.images[0].url;
+                let peticionCreate:any= this.http
+                .post(postUrl, product, {
+                  headers: new HttpHeaders({ "Content-Type": "application/json" })
+                })
+                .subscribe((product2:any) => {
+                }
+                );
+              }
+              console.log(image);
+              //this.createImage(image);
+            });
+          })
+        )
+        .subscribe();
+    }); 
+
+    /*let peticionCreate:any= await this.http
+    .post(postUrl, product, {
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    })
+    .subscribe((product2:any) => {
+    }
+    );*/
+}
 
   createImage(image:any){
     let postUrl: string = `${this.url}Images/`;
